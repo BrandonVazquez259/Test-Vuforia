e3337d5ec58ec0b3013ea3b3b6578c4415ed014a
BrandonVazquez259 <brandon.gonzalez@uteca.edu.mx>
2025-05-13 20:42:47 -0600

    Boton Con Baile

---
+++ Assets/scrips/InstantiateObject.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    
+    [SerializeField]
+    private GameObject _objectToInstantiate;
+
+ public void Instantiate(Transform target)
+    {
+        Instantiate(_objectToInstantiate, target.position, Quaternion.identity);
+    }
+}
+
+
